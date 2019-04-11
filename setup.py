ab70f3a20cde456c211721eb3a5e2ca956dc73da
graves <graves@ualberta.ca>
2019-04-11 15:27:44 -0600

    Update setup.py file.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='off-policy-actor-critic',
-      version='0.1',
-      url='http://github.com/gravesec/off-policy-actor-critic',
-      packages=find_packages(exclude=['test','test.*']),
+setup(name='actor-critic-with-emphatic-weightings',
+      version='0.0.1',
+      url='http://github.com/gravesec/actor-critic-with-emphatic-weightings',
+      packages=find_packages(exclude=['notebooks']),
+      description=('Source code for the paper "An Off-policy Policy Gradient Theorem Using Emphatic Weightings"')
       )
