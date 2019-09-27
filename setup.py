1d38674c27f88f4b90adda8b48bf0664f6daadfd
graves <graves@ualberta.ca>
2019-09-27 17:50:18 -0600

    Remove the setup.py file; this isn't really a package.

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='actor-critic-with-emphatic-weightings',
-      version='0.0.1',
-      url='http://github.com/gravesec/actor-critic-with-emphatic-weightings',
-      packages=find_packages(exclude=['notebooks', 'compute_canada', 'data', 'neurips_experiments', 've']),
-      description=('Source code for the paper "An Off-policy Policy Gradient Theorem Using Emphatic Weightings"')
-      )
