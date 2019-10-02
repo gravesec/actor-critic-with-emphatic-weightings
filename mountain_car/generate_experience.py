44ae1984af363dd441e2d31517dfb84a9a7c7ecd
graves <graves@ualberta.ca>
2019-10-01 18:05:11 -0600

    Minor changes.

--- mountain_car/generate_experience.py
+++ mountain_car/generate_experience.py
@@ -8,7 +8,6 @@ from joblib import Parallel, delayed
 
 # TODO: Figure out how to do checkpointing (i.e. keep track of progress via a memmap so if the process gets killed it can pick up where it left off).
 # TODO: Figure out how to append to a memmap in case we want to do more runs later on (we might get this without any extra work with checkpointing).
-# TODO: Move to JSON (away from argparse format) for experiment configuration?
 
 transition_dtype = np.dtype([('s_t', float, (2,)), ('a_t', int, 1), ('r_tp1', float, 1), ('s_tp1', float, (2,)), ('terminal', bool, 1)])
 
@@ -28,7 +27,7 @@ def generate_experience(experience, run_num, num_timesteps, random_seed):
     for t in range(num_timesteps):
 
         # Select an action:
-        a_t = rng.choice(env.action_space.n) # equiprobable random policy
+        a_t = rng.choice(env.action_space.n)  # equiprobable random policy
 
         # Take action a_t, observe next state s_tp1 and reward r_tp1:
         s_tp1, r_tp1, terminal, _ = env.step(a_t)
@@ -38,7 +37,7 @@ def generate_experience(experience, run_num, num_timesteps, random_seed):
             s_tp1 = env.reset()
 
         # Add the transition:
-        transitions[t] = (s_t, a_t, s_tp1, r_tp1, terminal)
+        transitions[t] = (s_t, a_t, r_tp1, s_tp1, terminal)
 
         # Update temporary variables:
         s_t = s_tp1
@@ -56,7 +55,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_timesteps', type=int, default=100000, help='The number of timesteps of experience to generate per run')
     parser.add_argument('--random_seed', type=int, default=3139378768, help='The master random seed to use')
     parser.add_argument('--num_cpus', type=int, default=-1, help='The number of cpus to use (-1 means all)')
-    parser.add_argument('--backend', type=str, default='loky', help='The backend to use (\'loky\' for processes or \'threading\' for threads). Always use \'loky\' lol')
+    parser.add_argument('--backend', type=str, default='loky', help='The backend to use (\'loky\' for processes or \'threading\' for threads). Always use \'loky\' because Python threading is terrible.')
     args = parser.parse_args()
 
     # Generate the random seed for each run without replacement:
