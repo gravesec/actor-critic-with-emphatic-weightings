a5d26525506dbb38fe6f6bfeb2423d801a2b58c5
graves <graves@ualberta.ca>
2019-10-25 11:29:02 -0600

    Move linear GTD to its own file.

--- mountain_car/ace.py
+++ mountain_car/ace.py
@@ -61,28 +61,6 @@ class ACE:
         self.rho_tm1 = rho_t
 
 
-class GTD:
-
-    def __init__(self, num_features, alpha_c, lambda_c):
-        self.alpha_c = alpha_c
-        self.alpha_w = alpha_c / 10.
-        self.lambda_c = lambda_c
-        self.e = np.zeros(num_features)
-        self.v = np.zeros(num_features)
-        self.w = np.zeros(num_features)
-
-    def learn(self, delta_t, indices_t, gamma_t, indices_tp1, gamma_tp1, rho_t):
-        self.e *= rho_t * gamma_t * self.lambda_c
-        self.e[indices_t] += rho_t
-        self.v += self.alpha_c * delta_t * self.e
-        self.v[indices_tp1] -= self.alpha_c * gamma_tp1 * (1 - self.lambda_c) * self.e.dot(self.w)
-        self.w[indices_t] -= self.alpha_w * self.w[indices_t].sum()
-        self.w += self.alpha_w * delta_t * self.e
-
-    def estimate(self, indices):
-        return self.v[indices].sum()
-
-
 class TOETD:
     """
     True Online Emphatic Temporal Difference learning algorithm based on code by Ashique Rupam Mahmood.
