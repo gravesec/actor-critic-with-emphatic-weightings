30ca03a7cf2b308130c059630cb6e06e785ce8ec
graves <graves@ualberta.ca>
2019-10-25 11:19:47 -0600

    Change linear GTD implementation to use indices instead of full feature vectors.

--- mountain_car/ace.py
+++ mountain_car/ace.py
@@ -63,22 +63,24 @@ class ACE:
 
 class GTD:
 
-    def __init__(self, num_features, alpha_v, lambda_c):
-        self.alpha_v = alpha_v
-        self.alpha_w = alpha_v / 10
+    def __init__(self, num_features, alpha_c, lambda_c):
+        self.alpha_c = alpha_c
+        self.alpha_w = alpha_c / 10.
         self.lambda_c = lambda_c
-
         self.e = np.zeros(num_features)
         self.v = np.zeros(num_features)
         self.w = np.zeros(num_features)
 
-    def learn(self, delta_t, x_t, gamma_t, x_tp1, gamma_tp1, rho_t):
-        self.e = rho_t * (gamma_t * self.lambda_c * self.e + x_t)
-        self.v += self.alpha_v * (delta_t * self.e - gamma_tp1 * (1 - self.lambda_c) * self.e.dot(self.w) * x_tp1)
-        self.w += self.alpha_w * (delta_t * self.e - x_t.dot(self.w) * x_t)
+    def learn(self, delta_t, indices_t, gamma_t, indices_tp1, gamma_tp1, rho_t):
+        self.e *= rho_t * gamma_t * self.lambda_c
+        self.e[indices_t] += rho_t
+        self.v += self.alpha_c * delta_t * self.e
+        self.v[indices_tp1] -= self.alpha_c * gamma_tp1 * (1 - self.lambda_c) * self.e.dot(self.w)
+        self.w[indices_t] -= self.alpha_w * self.w[indices_t].sum()
+        self.w += self.alpha_w * delta_t * self.e
 
-    def estimate(self, x):
-        return self.v.dot(x)
+    def estimate(self, indices):
+        return self.v[indices].sum()
 
 
 class TOETD:
