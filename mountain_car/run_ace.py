d840941a98805eb2a5e061fdfdb0800fc16d32d9
graves <graves@ualberta.ca>
2019-11-20 17:36:08 -0700

    Add learning rate for second set of weights to run_ace script.

--- mountain_car/run_ace.py
+++ mountain_car/run_ace.py
@@ -12,10 +12,14 @@ from mountain_car.generate_experience import num_actions, min_state_values, max_
 
 # TODO: Figure out how to do checkpointing (i.e. keep track of progress via a memmap so if the process gets killed it can pick up where it left off).
 # TODO: Figure out how to append to a memmap in case we want to do more runs later on (we might get this without any extra work with checkpointing).
+# TODO?: Refactor memmap layout to be optimal.
+# TODO?: Refactor run_ace into two functions:
+#  1. a function to run ace without side effects or knowledge of the layout of the results memmap (run_ace)
+#  2. a function to call run_ace and store returned results in the memmap
 
 
 def run_ace(policies, experience, behaviour_policy, checkpoint_interval, num_features, interest_function, run_num, config_num, parameters):
-    gamma, alpha_a, alpha_c, lambda_c, eta, num_tiles, num_tilings, bias_unit = parameters
+    gamma, alpha_a, alpha_c, alpha_w, lambda_c, eta, num_tiles, num_tilings, bias_unit = parameters
 
     # Create the interest function to use:
     i = eval(interest_function)
@@ -26,7 +30,7 @@ def run_ace(policies, experience, behaviour_policy, checkpoint_interval, num_fea
     # Create the agent:
     tc = TileCoder(min_state_values, max_state_values, [int(num_tiles), int(num_tiles)], int(num_tilings), num_features, int(bias_unit))
     actor = ACE(num_actions, num_features)
-    critic = BinaryTDC(num_features, alpha_c, alpha_c / 100., lambda_c)
+    critic = BinaryTDC(num_features, alpha_c, alpha_w, lambda_c)
 
     # Get the run of experience to learn from:
     transitions = experience[run_num]
@@ -38,7 +42,7 @@ def run_ace(policies, experience, behaviour_policy, checkpoint_interval, num_fea
 
         # Save the learned policy if it's a checkpoint timestep:
         if t % checkpoint_interval == 0:
-            policies[run_num, config_num, t // checkpoint_interval] = (gamma, alpha_a, alpha_c, lambda_c, eta, num_tiles, num_tilings, bias_unit, t, np.copy(actor.theta))
+            policies[run_num, config_num, t // checkpoint_interval] = (gamma, alpha_a, alpha_c, alpha_w, lambda_c, eta, num_tiles, num_tilings, bias_unit, t, np.copy(actor.theta))
 
         s_t, a_t, r_tp1, s_tp1, terminal = transition
 
@@ -71,7 +75,7 @@ def run_ace(policies, experience, behaviour_policy, checkpoint_interval, num_fea
         gamma_t = gamma_tp1
 
     # Save the policy after the final timestep:
-    policies[run_num, config_num, -1] = (gamma, alpha_a, alpha_c, lambda_c, eta, num_tiles, num_tilings, bias_unit, t+1, np.copy(actor.theta))
+    policies[run_num, config_num, -1] = (gamma, alpha_a, alpha_c, alpha_w, lambda_c, eta, num_tiles, num_tilings, bias_unit, t+1, np.copy(actor.theta))
 
 
 if __name__ == '__main__':
@@ -85,7 +89,7 @@ if __name__ == '__main__':
     parser.add_argument('--interest_function', type=str, default='lambda s, g=1: 1. if g==0. else 0.', help='Interest function to use. Example: \'lambda s, g=1: 1.\' (uniform interest function)')
     parser.add_argument('--behaviour_policy', type=str, default='lambda s: np.array([1/3, 1/3, 1/3])', help='Policy used to generate data. Example: \'lambda s: np.array([.9, .05, .05]) if s[1] < 0 else np.array([.05, .05, .9]) \' (energy pumping policy w/ 15 percent randomness)')
     parser.add_argument('--num_features', type=int, default=100000, help='The number of features to use in the tile coder.')
-    parser.add_argument('--parameters', type=float, nargs=8, action='append', metavar=('DISCOUNT_RATE', 'ACTOR_STEP_SIZE', 'CRITIC_STEP_SIZE', 'CRITIC_TRACE_DECAY_RATE', 'OFFPAC_ACE_TRADEOFF', 'NUM_TILES', 'NUM_TILINGS', 'BIAS_UNIT'), help='Parameters to use for ACE. Can be specified multiple times to run multiple configurations of ACE at once.')
+    parser.add_argument('--parameters', type=float, nargs=9, action='append', metavar=('DISCOUNT_RATE', 'ACTOR_STEP_SIZE', 'CRITIC_STEP_SIZE', 'CRITIC_STEP_SIZE_2', 'CRITIC_TRACE_DECAY_RATE', 'OFFPAC_ACE_TRADEOFF', 'NUM_TILES', 'NUM_TILINGS', 'BIAS_UNIT'), help='Parameters to use for ACE. Can be specified multiple times to run multiple configurations of ACE at once.')
     args = parser.parse_args()
 
     # Save the command line arguments in a format interpretable by argparse:
@@ -103,6 +107,7 @@ if __name__ == '__main__':
             ('gamma', float),
             ('alpha_a', float),
             ('alpha_c', float),
+            ('alpha_c2', float),
             ('lambda_c', float),
             ('eta', float),
             ('num_tiles', int),
