12df5de926ba29d031e7f34c863088522d2a22ab
graves <graves@ualberta.ca>
2020-02-28 14:40:29 -0700

    Update compute canada script.

--- compute_canada/ace_mc_sweep.sh
+++ compute_canada/ace_mc_sweep.sh
@@ -5,19 +5,21 @@
 #SBATCH --mail-user=graves@ualberta.ca
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH --time=00-01:00:00     # DD-HH:MM:SS
+#SBATCH --cpus-per-task=80  # Change to 80 to use hyperthreading.
+#SBATCH --time=00-01:00:00  # DD-HH:MM:SS
 
-# Prepare virtual environment on compute node:
+module load python/3.7
+
+# Configure virtual environment locally:
 virtualenv --no-download $SLURM_TMPDIR/ve
 source $SLURM_TMPDIR/ve/bin/activate
-pip install --no-index -r ~/actor-critic-with-emphatic-weightings/requirements.txt
+pip install --no-index --upgrade pip
+pip install --no-index -r $HOME/actor-critic-with-emphatic-weightings/requirements.txt
 
-# Generate data on compute node:
-python ~/actor-critic-with-emphatic-weightings/generate_experience.py \
---experiment_name $SLURM_TMPDIR/ace_mc_sweep \
---num_runs 30 \
---num_timesteps 200000 \
+python $HOME/actor-critic-with-emphatic-weightings/generate_experience.py \
+--experiment_name $SCRATCH/actor-critic-with-emphatic-weightings/ace_mc_sweep \
+--num_runs 10 \
+--num_timesteps 100000 \
 --random_seed 3139378768 \
 --num_cpus -1 \
 --backend "loky" \
@@ -25,9 +27,8 @@ python ~/actor-critic-with-emphatic-weightings/generate_experience.py \
 --behaviour_policy "lambda s: np.ones(env.action_space.n)/env.action_space.n" \
 --environment "MountainCar-v0"
 
-# Run ace parameter sweep, saving to compute node:
-python ~/actor-critic-with-emphatic-weightings/run_ace.py \
---experiment_name $SLURM_TMPDIR/ace_mc_sweep \
+python $HOME/actor-critic-with-emphatic-weightings/run_ace.py \
+--experiment_name $SCRATCH/actor-critic-with-emphatic-weightings/ace_mc_sweep \
 --checkpoint_interval 5000 \
 --num_cpus -1 \
 --backend "loky" \
@@ -37,18 +38,18 @@ python ~/actor-critic-with-emphatic-weightings/run_ace.py \
 --environment "MountainCar-v0" \
 --run_mode "combinations" \
 --gamma 1.0 \
---alpha_a 0.01 \
---alpha_c 0.05 \
---alpha_c2 0.0001 \
---lambda_c 0.0 \
---eta 0.0 \
+--alpha_a .00001 .00005 .0001 .0005 .001 .005 .01 .05 .1 \
+--alpha_c .00001 .00005 .0001 .0005 .001 .005 .01 .05 .1 \
+--alpha_c2 .0 .00001 .00005 .0001 .0005 .001 .005 .01 .05 .1 \
+--lambda_c .0 .4 .7 .9 .99 \
+--eta 0. 1. \
 --num_tiles 9 \
 --num_tilings 9 \
 --num_features 100000 \
 --bias_unit 1
 
-python ~/actor-critic-with-emphatic-weightings/evaluate_policies.py \
---experiment_name $SLURM_TMPDIR/ace_mc_sweep \
+python $HOME/actor-critic-with-emphatic-weightings/evaluate_policies.py \
+--experiment_name $SCRATCH/actor-critic-with-emphatic-weightings/ace_mc_sweep \
 --num_evaluation_runs 5 \
 --max_timesteps 1000 \
 --random_seed 1944801619 \
@@ -57,6 +58,3 @@ python ~/actor-critic-with-emphatic-weightings/evaluate_policies.py \
 --verbosity 0 \
 --objective "episodic" \
 --environment "MountainCar-v0"
-
-# Copy the results to the home directory:
-tar -cf ~/actor-critic-with-emphatic-weightings/ace_mc_sweep.tar $SLURM_TMPDIR/ace_mc_sweep
