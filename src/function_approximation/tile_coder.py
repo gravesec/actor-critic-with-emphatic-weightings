96cfc377ef61b1005bf37abf5957c34edc33e6e3
graves <graves@ualberta.ca>
2020-06-02 00:38:02 -0600

    Finish rewriting tile coder.

--- src/function_approximation/tile_coder.py
+++ src/function_approximation/tile_coder.py
@@ -1,44 +1,33 @@
 import numpy as np
 
 
-# TODO: implement bias unit.
 class TileCoder:
-    def __init__(self, limits=[[-10, 10], [-10, 10]], tiles_per_dim=[6, 6], num_tilings=4, bias_unit=True):
-        self.limits = np.asarray(limits)
-        self.tiles_per_dim = np.asarray(tiles_per_dim)
-        self.num_tilings = num_tilings
-        # self.bias_unit = bias_unit
+    def __init__(self, space, num_tiles_per_dim, num_tilings, bias_unit=False):
+        self.space = np.asarray(space)
+        self.num_tiles_per_dim = np.asarray(num_tiles_per_dim)
+        self.num_tilings = int(num_tilings)
+        self.bias_unit = bias_unit
+        self.num_active_features = self.num_tilings + self.bias_unit
+        self.total_num_tiles = self.num_tilings * np.prod(self.num_tiles_per_dim) + self.bias_unit
+        self.tile_size = (self.space[:, 1] - self.space[:, 0]) / (self.num_tiles_per_dim - 1)
 
-        self.tiling_offsets = np.array([[(tiling_num / num_tilings * 3 ** dimension) % 1 for dimension in range(len(limits))] for tiling_num in range(num_tilings)])
-        self.scale_factors = (self.tiles_per_dim - 1) / (self.limits[:, 1] - self.limits[:, 0])
-        self.num_tiles = self.num_tilings * np.prod(self.tiles_per_dim)
+        # Compute the offset in each dimension for each tiling:
+        self.tiling_offsets = np.arange(1, 2 * len(self.space), 2) * np.repeat(np.arange(self.num_tilings), len(self.space)).reshape(self.num_tilings, len(self.space)) / self.num_tilings % 1 * self.tile_size
+        # Create an array to help convert n-dimensional tiling coordinates into a 1-dimensional index in the tiling:
+        self.coords_to_indices = np.array([np.prod(self.num_tiles_per_dim[0:dim]) for dim in range(len(self.space))])
+        # Compute the indices in the feature vector where each tiling starts:
+        self.tilings_to_features = np.prod(self.num_tiles_per_dim) * np.arange(self.num_tilings)
 
-        self.tile_base_ind = np.prod(self.tiles_per_dim) * np.arange(num_tilings)
-
-        self.hash_vec = np.array([np.prod(tiles_per_dim[0:i]) for i in range(len(limits))])
-
-    def indices(self, obs):
-        offset_coords = ((obs - self.limits[:, 0]) * self.scale_factors + self.tiling_offsets).astype(int)
-        return self.tile_base_ind + np.dot(offset_coords, self.hash_vec)
-
-
-class KrisTileCoder:
-    def __init__(self, tiles_per_dim, value_limits, tilings, offset=lambda n: 2 * np.arange(n) + 1):
-        tiling_dims = np.array(np.ceil(tiles_per_dim), dtype=np.int) + 1
-        self._offsets = offset(len(value_limits)) * \
-          np.repeat([np.arange(tilings)], len(value_limits), 0).T / float(tilings) % 1
-        self._limits = np.array(value_limits)
-        self._norm_dims = np.array(tiles_per_dim) / (self._limits[:, 1] - self._limits[:, 0])
-        self._tile_base_ind = np.prod(tiling_dims) * np.arange(tilings)
-        self._hash_vec = np.array([np.prod(tiling_dims[0:i]) for i in range(len(value_limits))])
-        self._n_tiles = tilings * np.prod(tiling_dims)
-
-    def __getitem__(self, x):
-        off_coords = ((x - self._limits[:, 0]) * self._norm_dims + self._offsets).astype(int)
-        return self._tile_base_ind + np.dot(off_coords, self._hash_vec)
+    def encode(self, obs):
+        # Compute the coordinates in each tiling of the tile containing the observation:
+        tiling_coords = ((np.asarray(obs) - self.space[:, 0] + self.tiling_offsets) // self.tile_size).astype(int)
+        # Convert the N-dimensional tiling coordinates to a 1-dimensional index in the tiling:
+        tiling_indices = np.dot(tiling_coords, self.coords_to_indices)
+        # Convert the tiling indices to indices in the feature vector:
+        feature_indices = self.tilings_to_features + tiling_indices
+        return np.append(feature_indices, self.total_num_tiles - 1) if self.bias_unit else feature_indices
 
 
-# TODO: update test for tile coder.
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
@@ -51,15 +40,16 @@ if __name__ == '__main__':
         return y + .1 * np.random.randn()
 
     # Create a tile coder and weight vector:
-    tc = TileCoder(limits=[[-10, 10], [-10, 10]], tiles_per_dim=[6, 6], num_tilings=4, bias_unit=True)
-    w = np.zeros(tc.num_observation_features)
+    tc = TileCoder(space=[[-10, 10], [-10, 10]], num_tiles_per_dim=[11, 11], num_tilings=8, bias_unit=True)
+
+    w = np.zeros(tc.total_num_tiles)
     step_size = .2 / tc.num_active_features
 
     # Use the tile coder to train the weight vector from samples of the target function:
     num_examples = 1000
     for i in range(num_examples):
         x1, x2 = np.random.random_sample(2) * 20 - 10  # Sample the input space uniformly.
-        indices = tc.get_indices((x1, x2))  # Compute indices for the input point.
+        indices = tc.encode((x1, x2))  # Compute indices for the input point.
         y_hat = w[indices].sum()  # Generate an estimate from the weights.
         y = noise(target_function(x1, x2))  # Get a sample output from the function.
         w[indices] += step_size * (y - y_hat)  # Update the weight vector.
@@ -73,7 +63,7 @@ if __name__ == '__main__':
     for j in range(len(x1)):
         for k in range(len(x2)):
             y[j, k] = target_function(x1[j], x2[k])  # True value of the function.
-            y_hat[j, k] = w[tc.get_indices((x1[j], x2[k]))].sum()  # Learned estimate.
+            y_hat[j, k] = w[tc.encode((x1[j], x2[k]))].sum()  # Learned estimate.
 
     # Visualize the function and the learned approximation:
     x1, x2 = np.meshgrid(x1, x2)
