87d4434647a5cedc7ac00dccc94986c3cc483415
graves <graves@ualberta.ca>
2020-06-02 00:51:24 -0600

    Update tile coder test and remove tiles3.py

--- test/function_approximation/test_tile_coder.py
+++ test/function_approximation/test_tile_coder.py
@@ -9,39 +9,57 @@ class TileCoderTests(unittest.TestCase):
 
     def test_tile_coder(self):
         np.random.seed(2734287609)
-        num_features = 1024
-        weights = np.zeros(num_features)
-        tc = TileCoder([0, 0], [2 * np.pi, 2 * np.pi], 8, 8, num_features, True)
-        step_size = .1/tc.num_active_features
 
-        def target(x, y):
-            return np.sin(x) + np.cos(y) + 0.1 * np.random.randn()
+        # Define a function to approximate:
+        def target_function(x1, x2):
+            return np.sin(np.sqrt(x1**2 + x2**2)) / np.sqrt(x1**2 + x2**2)
 
-        # Use the tilecoder to train:
-        for i in range(10000):
-            x, y = 2 * np.pi * np.random.rand(2)
-            z = target(x, y)
-            indices = tc.indices((x, y))
-            weights[indices] += step_size * (z - weights[indices].sum())
+        # A function to add noise:
+        def noise(y):
+            return y + .1 * np.random.randn()
 
+        # Create a tile coder and weight vector:
+        tc = TileCoder(space=[[-10, 10], [-10, 10]], num_tiles_per_dim=[11, 11], num_tilings=8, bias_unit=True)
+        weights = np.zeros(tc.total_num_tiles)
+        step_size = .2 / tc.num_active_features
+
+        # Use the tile coder to train the weight vector from noisy samples of the target function:
+        num_examples = 1000
+        for i in range(num_examples):
+            x1, x2 = np.random.random_sample(2) * 20 - 10  # Sample the input space uniformly.
+            indices = tc.encode((x1, x2))  # Compute indices for the input point.
+            y_hat = weights[indices].sum()  # Generate an estimate from the weights.
+            y = noise(target_function(x1, x2))  # Get a noisy sample output from the function.
+            weights[indices] += step_size * (y - y_hat)  # Update the weight vector.
+
+        # Check the function and the learned approximation:
         resolution = 100
-        x = np.arange(0.0, 2.0 * np.pi, 2.0 * np.pi / resolution)
-        y = np.arange(0.0, 2.0 * np.pi, 2.0 * np.pi / resolution)
-        z = np.zeros((resolution, resolution))
-        z_hat = np.zeros((resolution, resolution))
-        for j in range(len(x)):
-            for k in range(len(y)):
-                z[j,k] = target(x[j], y[k])
-                z_hat[j,k] = weights[tc.indices((x[j],y[k]))].sum()
+        x1 = np.arange(-10, 10, 20 / resolution)
+        x2 = np.arange(-10, 10, 20 / resolution)
+        y = np.zeros((resolution, resolution))
+        y_hat = np.zeros((resolution, resolution))
+        for j in range(len(x1)):
+            for k in range(len(x2)):
+                y[j, k] = target_function(x1[j], x2[k])  # True value of the function.
+                y_hat[j, k] = weights[tc.encode((x1[j], x2[k]))].sum()  # Learned estimate.
 
+        # Visualize the function and the learned approximation:
+        x1, x2 = np.meshgrid(x1, x2)
         fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        x, y = np.meshgrid(x, y)
-        surface = ax.plot_surface(x, y, z_hat, cmap='hot')
-        ax.plot_surface(x, y, z, color=(0.1, 0.2, 0.5, 0.3))
-        plt.savefig('tile_coder_surface.png')
-        tolerance = .1
-        self.assertLess(np.mean(z - z_hat), tolerance)
+
+        ax = fig.add_subplot(1, 2, 1, projection='3d')
+        ax.plot_surface(x1, x2, y_hat, cmap='hot')
+        ax.set_zlim(-.25, 1.)
+        ax.set_title('Learned estimate after {} examples'.format(num_examples))
+
+        ax = fig.add_subplot(1, 2, 2, projection='3d')
+        ax.plot_surface(x1, x2, y, cmap='hot')
+        ax.set_zlim(-.25, 1.)
+        ax.set_title('True function')
+        plt.savefig('tile_coder_test.png')
+
+        tolerance = .01
+        self.assertLess(np.mean(y - y_hat), tolerance)
 
 
 if __name__ == '__main__':
