71059371e2906832062de3d3fde50914a89a97d4
graves <graves@ualberta.ca>
2020-09-14 13:59:12 -0600

    Updating sweep script.

--- compute_canada/sweep.py
+++ compute_canada/sweep.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     parser.add_argument('--experience_file', type=str, default='experience.npy', help='The file to read experience from')
     parser.add_argument('--num_cpus', type=int, default=-1, help='The number of cpus to use (-1 for all).')
 
-    parser.add_argument('--num_runs', type=int, default=5, help='The number of independent runs to do')
-    parser.add_argument('--checkpoint_interval', type=int, default=5000, help='The number of timesteps after which to save the learned policy.')
-    parser.add_argument('--num_evaluation_runs', type=int, default=5, help='The number of times to evaluate each policy')
-    parser.add_argument('--max_timesteps', type=int, default=5000, help='The maximum number of timesteps allowed per policy evaluation')
+    parser.add_argument('--num_runs', type=int, default=10, help='The number of independent runs to do')
+    parser.add_argument('--checkpoint_interval', type=int, default=1000, help='The number of timesteps after which to save the learned policy.')
+    parser.add_argument('--num_evaluation_runs', type=int, default=10, help='The number of times to evaluate each policy')
+    parser.add_argument('--max_timesteps', type=int, default=1000, help='The maximum number of timesteps allowed per policy evaluation')
     parser.add_argument('--random_seed', type=int, default=735026919, help='The master random seed to use')
 
     parser.add_argument('--script_name', type=str, default='run_ace.py', help='The script to run on each node.')
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('--interest_function', type=str, default='lambda s, g=1: 1.', help='Interest function to use. Example: \'lambda s, g=1: 1. if g==0. else 0.\' (episodic interest function)')
     parser.add_argument('--behaviour_policy', type=str, default='lambda s: np.ones(env.action_space.n)/env.action_space.n', help='Policy to use. Default is uniform random. Another Example: \'lambda s: np.array([.9, .05, .05]) if s[1] < 0 else np.array([.05, .05, .9]) \' (energy pumping policy w/ 15 percent randomness)')
     parser.add_argument('--environment', type=str, default='MountainCar-v0', help='An OpenAI Gym environment string.')
-    parser.add_argument('--gamma', '--discount_rate', type=float, default=.99, help='Discount rate.')
+    parser.add_argument('--gamma', '--discount_rate', type=float, default=.95, help='Discount rate.')
     parser.add_argument('--alpha_a', type=float, nargs='+', default=[1/2**i for i in range(15)], help='Step sizes for the actor.')
     parser.add_argument('--alpha_w', type=float, nargs='+', default=[1/2**i for i in range(15)], help='Step sizes for the critic.')
     parser.add_argument('--alpha_v', type=float, nargs='+', default=[1/2**i for i in range(15)], help='Step sizes for the critic\'s auxiliary weights.')
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_hours', type=int, default=1, help='Number of hours the job should run for. On Niagara consider using one of: 1, 3, 12, 24.')
     parser.add_argument('--email', type=str, default='graves@ualberta.ca', help='Email address to report updates to.')
     parser.add_argument('--account', type=str, default='def-sutton', help='Allocation string to use in slurm.')
-    parser.add_argument('--cores_per_node', type=int, default=40, help='Number of cores per node on the cluster. Niagara is 40.')
+    parser.add_argument('--cores_per_node', type=int, default=80, help='Number of cores per node on the cluster. Niagara is 40.')
     args = parser.parse_args()
 
     # Calculate how many nodes to use:
@@ -68,12 +68,13 @@ if __name__ == '__main__':
 #SBATCH --mail-type=ALL
 #SBATCH --account={args.account}
 #SBATCH --job-name=sweep{script_num}
+#SBATCH --output=./sweep{script_num}/slurm-%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task={args.cores_per_node}
 #SBATCH --time=00-{args.num_hours}:00:00  # DD-HH:MM:SS
 module load python/3.6.8
-source ../../ve/bin/activate
-python ../../{args.script_name} \\
+source $SCRATCH/actor-critic-with-emphatic-weightings/ve/bin/activate
+python $SCRATCH/actor-critic-with-emphatic-weightings/{args.script_name} \\
 --output_dir \'sweep{script_num}\' \\
 --experience_file \'{args.experience_file}\' \\
 --num_cpus {args.num_cpus} \\
@@ -97,10 +98,13 @@ python ../../{args.script_name} \\
             script_file.write(script)
             os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC)  # Make script executable.
 
+        # Create the output directory for the script:
+        os.makedirs(output_dir / f'sweep{script_num}', exist_ok=True)
+
     if input('Schedule jobs now? y/[n]: ') != 'y':
         exit(0)
 
-    os.chdir(args.output_dir)
+    os.chdir(output_dir)
     for script_name in script_names:
         os.system(f'sbatch {script_name}')
         time.sleep(1)
