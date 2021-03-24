bb64062e02303626eb0e36124fbe59e0f9cbc364
rakshakumar23 <raksha.kumar.rd@gmail.com>
2021-03-23 18:18:49 -0600

    added tdrc

---
+++ src/algorithms/tdrc.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class BinaryTDRC:
+
+    def __init__(self, num_features, alpha , lambda_c):
+        self.num_features = num_features
+        self.alpha_w = alpha
+        self.alpha_v = self.alpha_w
+        self.beta = 1.0
+        self.lambda_c = lambda_c
+        self.w = np.zeros(num_features)
+        self.v = np.zeros(num_features)
+        self.z = np.zeros(num_features)
+
+    def learn(self, delta_t, indices_t, gamma_t, indices_tp1, gamma_tp1, rho_t):
+        self.z *= rho_t * gamma_t * self.lambda_c
+        self.z[indices_t] += rho_t
+        self.w += self.alpha_w * delta_t * self.z
+        self.w[indices_tp1] -= self.alpha_w * gamma_tp1 * (1 - self.lambda_c) * self.z.dot(self.v)
+        v_dot_x = self.v[indices_t].sum()
+        self.v += ((self.alpha_v * delta_t * self.z) - (self.alpha_v * self.beta * self.v))
+        self.v[indices_t] -= self.alpha_v * v_dot_x
+
+    def estimate(self, indices):
+        return self.w[indices].sum()
